29b2c9357abd5e6dcd494119477a3568adcdfd11
Mark Granade <88593585+MarkGranade@users.noreply.github.com>
2022-01-22 21:00:57 -0500

    Finished Lesson 2

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,22 @@
 const { User, Thought } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
 	Query: {
+		me: async (parent, args, context) => {
+			if (context.user) {
+				const userData = await User.findOne({ _id: context.user._id })
+					.select("-__v -password")
+					.populate("thoughts")
+					.populate("friends");
+
+				return userData;
+			}
+
+			throw new AuthenticationError("Not logged in");
+		},
+
 		// We pass in the "parent" as more of a placeholder parameter. It won't be used, but we need something in that first parameter's spot so we can access the "username" argument from the second parameter.
 		thoughts: async (parent, { username }) => {
 			// Ternary operator to check if "username" exists. If it does, we set "params" to an object with a "username" key set to that value. If it doesn't, we return an empty object.
@@ -27,6 +42,84 @@ const resolvers = {
 				.select("-__v -password")
 				.populate("friends")
 				.populate("thoughts");
+		},
+	},
+	Mutation: {
+		addUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+
+			return { token, user };
+		},
+
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+
+			const token = signToken(user);
+			return { token, user };
+		},
+
+		addThought: async (parent, args, context) => {
+			if (context.user) {
+				const thought = await Thought.create({
+					...args,
+					username: context.user.username,
+				});
+
+				await User.findByIdAndUpdate(
+					{ _id: context.user._id },
+					{ $push: { thoughts: thought._id } },
+					{ new: true }
+				);
+
+				return thought;
+			}
+
+			throw new AuthenticationError("You need to be logged in!");
+		},
+
+		addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+			if (context.user) {
+				const updatedThought = await Thought.findOneAndUpdate(
+					{ _id: thoughtId },
+					// Reactions are stored as arrays on the "Thought" model, so we'll use the Mongo "$push" operator.
+					{
+						$push: {
+							reactions: { reactionBody, username: context.user.username },
+						},
+					},
+					{ new: true, runValidators: true }
+				);
+
+				return updatedThought;
+			}
+
+			throw new AuthenticationError("You need to be logged in!");
+		},
+
+		addFriend: async (parent, { friendId }, context) => {
+			if (context.user) {
+				const updatedUser = await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					// A user can't be friends with the same person twice, so we use the $addToSet operator instead of $push to prevent duplicate entries.
+					{ $addToSet: { friends: friendId } },
+					{ new: true }
+				).populate("friends");
+
+				return updatedUser;
+			}
+
+			throw new AuthenticationError("You need to be logged in!");
 		},
 	},
 };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -14,7 +15,8 @@ const startServer = async () => {
 	const server = new ApolloServer({
 		typeDefs,
 		resolvers,
-		// context: authMiddleware,
+		// This ensures that every request performs an authentication check, and the updated request object will be passed to the resolvers as the "context"
+		context: authMiddleware,
 	});
 
 	// Start the Apollo server
